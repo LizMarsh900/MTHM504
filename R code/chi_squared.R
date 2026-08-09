684799e417645208ed31759d09c4a92bf5a84bcd
LizMarsh900 <em900@exeter.ac.uk>
2026-08-09 11:28:10 +0100

    Neatened up

--- R code/chi_squared.R
+++ R code/chi_squared.R
@@ -1,11 +1,12 @@
 library(dplyr)
 library(purrr)
+library(excel.link)
 
 # Set working directory  to folder with data - change as necessary
 setwd("C:/Users/Liz/OneDrive - University of Exeter/MSc/Summer Project/data_copy")
 
 # Load clean data (if not already loaded)
-d <- readRDS("data_clean.rds")
+d <- readRDS("data_revised_clean.rds")
 
 # Load .gov data - gives the number of births (in thousands) per year and month
 # See table 2.3 at https://assets.publishing.service.gov.uk/media/5a7c89aae5274a0bb7cb7b1b/9787777148210.pdf
@@ -13,7 +14,7 @@ births <- xl.read.file(".gov_data.xlsx",
                    xl.sheet = "Sheet2")
 
 # Create variables for total births in each quarter, total each year
-# and find proportion
+# and find proportions
 births <- births %>%
   mutate(
     Q1 = Sep + Oct + Nov,
@@ -25,36 +26,16 @@ births <- births %>%
   )
 
 
-#### Chi-squared tests of applications IN GENERAL ##############################
+#### Chi-square comparing to census data #######################################
 
-# Specifically this is comparing birth quarter distributions to 25% for each
-# Would be more accurate to use actual census data for this
-
-# Making table of 2021-23 applicants for chi-square test
+# Making table of 2021-23 birth quarters and numbers in each
 obs2123 <- d %>%
   filter(year_group == "2021-23") %>%
   pull(birth_quarter) %>%
   table()
 
-# Perform chi-squared test
-test2123 <- chisq.test(obs2123)
-
-# Get residuals
-test2123$residuals
-
-# Same steps for 2022-2024
-obs2224 <- d %>%
-  filter(year_group == "2022-24") %>%
-  pull(birth_quarter) %>%
-  table()
-
-test2224 <- chisq.test(obs2224)
-
-test2224$residuals
-
-#### Slightly more technical by comparing to census data #######################
-
 # 2021-2023 cohort are born in 2004 and 2005, so average these census proportions
+# Create a table of averaged total live births
 expected2123 <- births %>%
   filter(Year %in% c(2004, 2005)) %>%
   summarise(
@@ -65,7 +46,7 @@ expected2123 <- births %>%
   ) %>%
   unlist()
 
-# Find proportions
+# Find proportions (instead of just counts)
 expected2123 <- expected2123 / sum(expected2123)
 
 # Conduct chi square test
@@ -75,7 +56,14 @@ chi2123$observed
 chi2123$expected
 chi2123$residuals
 
-# 2022-2024 cohort born in 2005 and 2006 same steps again
+
+#### Same steps again for 2022-2024 ####
+obs2224 <- d %>%
+  filter(year_group == "2022-24") %>%
+  pull(birth_quarter) %>%
+  table()
+
+# 2022-2024 cohort born in 2005 and 2006
 expected2224 <- births %>%
   filter(Year %in% c(2005, 2006)) %>%
   summarise(
@@ -93,52 +81,4 @@ chi2224 <- chisq.test(obs2224, p = expected2224)
 chi2224$observed
 chi2224$expected
 chi2224$residuals
-
-
-
-#### Now comparing selected versus non-selected ################################
-
-tab <- table(d$birth_quarter, d$selected)
-
-tab
-
-chisq.test(tab)
-
-
-# Broken down by year group
-d %>%
-  group_by(year_group) %>%
-  summarise(
-    test = list(chisq.test(table(birth_quarter, selected)))
-  ) %>%
-  mutate(
-    p_value = map_dbl(test, ~ .x$p.value),
-    chisq   = map_dbl(test, ~ .x$statistic)
-  )
-
-
-#### Gender ####################################################################
-
-# Create table for use in Chi-square test 
-tab <- table(d$sex, d$selected)
-
-# Test whether the sex distribution of selected applicants is same as whole pool
-chisq.test(tab)
-
-# "Prefer not to say" option caused issues in later chi-square test, 
-# Since only 1 applicant has picked it, we'll remove for now
-d2 <- d %>%
-  filter(sex %in% c("Female", "Male")) %>%
-  droplevels()
-
-# Same test as above but broken down by year
-d2 %>%
-  group_by(year_group) %>%
-  summarise(
-    test = list(chisq.test(table(sex, selected)))
-  ) %>%
-  mutate(
-    p_value = map_dbl(test, ~ .x$p.value),
-    chisq   = map_dbl(test, ~ .x$statistic)
-  )
 
