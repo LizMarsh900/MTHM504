0bf93d9e96ab1aa2cdc06292e8d979c6ddbe965f
LizMarsh900 <em900@exeter.ac.uk>
2026-08-12 17:44:39 +0100

    Update mapping.R

--- R code/mapping.R
+++ R code/mapping.R
@@ -8,12 +8,21 @@ library(tidyverse)
 library(PostcodesioR) # For getting lat and lon of postcodes
 library(spdep) 
 library(INLA) #For spatial smoothing later on
+library(showtext) #For getting Times New Roman font on figures
+
+#### Only needs to be run once ####
 
 # NOTE: INLA cannot be installed easily  through `install.packages`. Use this:
 install.packages("INLA",
                  repos = c(getOption("repos"),
                            INLA = "https://inla.r-inla-download.org/R/stable"),
                  dependencies = TRUE)
+
+# Get Times New Roman
+font_add("Times New Roman", regular = "C:/Windows/Fonts/times.ttf")
+showtext_auto()
+
+#####
 
 # Set working directory  to folder with data - change as necessary
 setwd("C:/Users/Liz/OneDrive - University of Exeter/MSc/Summer Project/data_copy")
@@ -181,9 +190,10 @@ ggplot(ctyua_map2224) +
   geom_sf(aes(fill = acceptance_rate), color = NA) +
   scale_fill_viridis_c(option = "D", na.value = "white") +
   theme_void() +
-  labs(fill = "Acceptance Rate") +
+  labs(fill = "Acceptance \nRate") +
   ggtitle("County and Unitary Authority District Acceptance Rates \nfor 2022-2024 Applicants") +
   theme(plot.title = element_text(hjust = 0.5),
+        text = element_text(family = "Times New Roman"),
         plot.background = element_rect(
           fill = "grey80",
           colour = NA))
@@ -205,6 +215,9 @@ hubs <- locations %>%
   st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
   st_transform(st_crs(ctyua_map2224))
 
+# Add dummy column for hubs df so that a legend can be created on figures
+hubs$hub_type <- "Regional Hubs"
+
 # Same CTYUA map as earlier but with all hubs on as an example
 ggplot(ctyua_map2123) +
   geom_sf(aes(fill = acceptance_rate), color = NA) +
@@ -220,8 +233,73 @@ ggplot(ctyua_map2123) +
   geom_sf(data = hubs, colour = "red", size = 4)
 
 
+#### Combined year group plots for write-up ####
+ctyua_acceptance <- ctyua_d %>%
+  group_by(CTYUA19CD) %>%
+  summarise(
+    applicants = n(),
+    accepted = sum(selected == "Yes"),
+    acceptance_rate = accepted / applicants)
 
+ctyua_map <- ctyua_ONS %>%
+  left_join(ctyua_acceptance, by = c("ctyua19cd" = "CTYUA19CD"))
 
+ggplot(ctyua_map) +
+  geom_sf(aes(fill = acceptance_rate), color = NA) +
+  scale_fill_viridis_c(option = "D", na.value = "white") +
+  theme_void() +
+  labs(fill = "Acceptance \nRate") +
+  ggtitle("County and Unitary Authority District Acceptance Rates") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+        text = element_text(family = "Times New Roman", size = 16),
+        plot.background = element_rect(
+          fill = "grey80",
+          colour = NA), 
+        legend.position = "left", legend.box.margin = margin(0, 0, 0, 30))  +
+  geom_sf(data = hubs, aes(shape = hub_type) ,colour = "black", 
+          fill = "red", size = 4) +
+  scale_shape_manual(
+    name = NULL,
+    values = c("Regional Hubs" = 24)
+  )
+
+#### Total map with LSOA centroids plotted for reference ####
+
+# Count number of applicants in each LSOA
+lsoa_counts<- d %>%
+  count(LSOA_code)
+
+# join to ONS data and find centroids
+lsoa_points <- lsoa_ONS %>%
+  inner_join(
+    lsoa_counts,
+    by = c("LSOA11CD" = "LSOA_code")
+  ) %>%
+  st_centroid()
+
+# Convert to factor since max two people from each LSOA
+lsoa_points$n <- factor(lsoa_points$n)
+
+# Plot
+ggplot() +
+  geom_sf(data = england, fill = "white", colour = "grey80") +
+  ggtitle("Applicant Lower Layer Super Output Areas") +
+  geom_sf(data = lsoa_points, aes(colour = n), size = 2, alpha = 0.7) +
+  scale_colour_manual(
+    values = c(
+      "1" = "#619CFF",
+      "2" = "#F8766D"
+    ),
+    name = "Applicants \nper LSOA"
+  ) + 
+  theme_void()   +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+        text = element_text(family = "Times New Roman", size = 16),
+        plot.background = element_rect(
+          fill = "grey80",
+          colour = NA),
+        legend.position = "left", legend.box.margin = margin(0, 0, 0, 30)
+  )
 
 #### SPATIAL SMOOTHING #########################################################
 
@@ -274,6 +352,29 @@ ggplot(ctyua_rawmap2224) +
   geom_sf(data = hubs, colour = "red", size = 4)
 
 
+#### Raw application counts for whole cohort ####
+ctyua_applications <- ctyua_d %>%
+  group_by(CTYUA19CD) %>%
+  summarise(
+    applications = n())
+
+ctyua_rawmap <- ctyua_ONS %>%
+  left_join(ctyua_applications, by = c("ctyua19cd" = "CTYUA19CD"))
+
+ggplot(ctyua_rawmap) +
+  geom_sf(aes(fill = applications), color = NA) +
+  scale_fill_viridis_c(option = "D", na.value = "white") +
+  theme_void() +
+  labs(fill = "Applications") +
+  ggtitle("County and Unitary Authority District Application Counts") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+        text = element_text(family = "Times New Roman", size = 16),
+        plot.background = element_rect(
+          fill = "grey80",
+          colour = NA), 
+        legend.position = "left", legend.box.margin = margin(0, 0, 0, 30))
+
+
 #### Smoothing process 2021-23 #####
 
 # Number of applications per CTYUA INCLUDING CTYUA CODES WITH ZERO APPLICANTS
@@ -283,7 +384,6 @@ d2123 <- ctyua_ONS %>%
   left_join(ctyua_applications_2123,
             by = c("ctyua19cd" = "CTYUA19CD")) %>%
   mutate(applications = replace_na(applications, 0))
-
 
 # Create neighbourhood list i.e. which counties share boundaries
 nb <- poly2nb(ctyua_ONS)
@@ -318,7 +418,7 @@ d2123$smoothed_applications <- model2123$summary.fitted.values$mean
 d2123$credible_lower <- model2123$summary.linear.predictor$`0.025quant` # Credible intervals
 d2123$credible_upper <- model2123$summary.linear.predictor$`0.975quant`
 
-# Join these back to your polygons.
+# Join these back to polygons
 ctyua_results2123 <- ctyua_ONS %>%
   left_join(d2123, by = "ctyua19cd")
 
@@ -333,3 +433,73 @@ ggplot(ctyua_results2123) +
         plot.background = element_rect(
           fill = "grey80",
           colour = NA)) 
+
+
+#### Smoothing process for whole cohort ####
+
+# Number of applications per CTYUA INCLUDING CTYUA CODES WITH ZERO APPLICANTS
+d_smooth <- ctyua_ONS %>%
+  st_drop_geometry() %>%
+  select(ctyua19cd) %>%
+  left_join(ctyua_applications,
+            by = c("ctyua19cd" = "CTYUA19CD")) %>%
+  mutate(applications = replace_na(applications, 0))
+
+# Create neighbourhood list i.e. which counties share boundaries
+nb <- poly2nb(ctyua_ONS)
+
+# Write graph for INLA format
+nb2INLA("ctyua.graph", nb)
+
+# Read graph into INLA
+graph <- inla.read.graph("ctyua.graph")
+
+# Fit the model
+d_smooth$area_id <- 1:nrow(d_smooth)
+
+model <- inla(
+  applications ~
+    f(area_id,
+      model = "bym2",
+      graph = graph),
+  family = "poisson",
+  data = d_smooth,
+  control.predictor = list(compute = TRUE),
+  control.compute = list(dic = TRUE, waic = TRUE)
+)
+
+# Extract posterior estimates - on the log scale:
+d_smooth$mean <- model$summary.linear.predictor$mean
+d_smooth$lower <- model$summary.linear.predictor$`0.025quant` # CIs
+d_smooth$upper <- model$summary.linear.predictor$`0.975quant`
+
+# Extract posterior estimates - on original scale:
+d_smooth$smoothed_applications <- model$summary.fitted.values$mean
+d_smooth$credible_lower <- model$summary.fitted.values$`0.025quant` # Credible intervals
+d_smooth$credible_upper <- model$summary.fitted.values$`0.975quant`
+
+# Find observed application count minus fitted
+# then positive numbers mean more applications observed than predicted
+# negative means fewer applications observed than predicted
+d_smooth$difference <- d_smooth$applications - d_smooth$smoothed_applications
+
+# Join these back to polygons
+ctyua_results <- ctyua_ONS %>%
+  left_join(d_smooth, by = "ctyua19cd")
+
+# Plot expected number of applications
+ggplot(ctyua_results) +
+  geom_sf(aes(fill = difference), colour = NA) +
+  scale_fill_viridis_c(option = "D", na.value = "white") +
+  labs(title = "Observed - Fitted Application Counts",
+       fill = "Observed -\nFitted") +
+  theme_void() +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+        text = element_text(family = "Times New Roman", size = 16),
+        plot.background = element_rect(
+          fill = "grey80",
+          colour = NA), 
+        legend.position = "left", legend.box.margin = margin(0, 0, 0, 30))
+
+# Find hyperparameters (e.g. overall spatial effect)
+model$summary.hyperpar
